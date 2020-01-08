8736e1e538c422ea5bf7625977df491a18427a93
Harshvardhan Singh <harshvardhansingh58@gmail.com>
2020-01-08 15:34:07 +0530

    mobx setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-const App = ({ title }) => (
-  <div>
-    {title}
-    {" ye hia to-do"}
-  </div>
-);
-export default App;
+import React, { Component } from "react";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+
+@observer
+export default class App extends Component {
+  @observable text = "hii";
+  render() {
+    return (
+      <div>
+        <p>{this.text}</p>
+        <input value={this.text} onChange={e => (this.text = e.target.value)} />
+        <button onClick={() => (this.text = "hii")}>Reset</button>
+      </div>
+    );
+  }
+}
